30bc00c2e230765274f819b4cda7bb0b067d4df2
Роман Соколов <145982699+ex3lite@users.noreply.github.com>
2025-08-29 11:55:29 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,51 @@
-import { EventsSDK } from "github.com/octarine-public/wrapper/index"
+import './i18n'
 
-console.log("Hello world!")
-EventsSDK.on("GameStarted", () => {
-	console.log("Hello world!")
+import { Events, Menu, LocalPlayer, GameState, EventsSDK, ImageData } from 'github.com/octarine-public/wrapper/index'
+
+const customGamesEntry = Menu.AddEntry('Custom Games', ImageData.Icons.icon_svg_keyboard)
+
+let customGameNode: Menu.Node | null = null
+customGamesEntry.AddShortDescription('supported_games')
+
+EventsSDK.on('GameStarted', () => {
+	switch (GameState.AddonName) {
+		case '2860562213': {
+			customGameNode = customGamesEntry.AddNode('custom_spintd')
+	
+			const goldSlider = customGameNode.AddSlider('Gold amount', 0, 0, 500, 0, 'Amount of gold to give', 1)
+			const giveGoldButton = customGameNode.AddButton('Give gold', 'Give gold to yourself', 1)
+	
+			giveGoldButton.OnValue(() => {
+				const gold = goldSlider.value
+	
+				CustomGameEvents.FireEventToServer('DebugAddGold', new Map<string, any>(Object.entries({
+					player_id: LocalPlayer!.PlayerID,
+					gold_number: gold
+				})))
+			})
+	
+			const allowBuy = customGameNode.AddToggle('Debug Buy', false, 'Ability of giving items from store for free', 2)
+	
+			allowBuy.OnValue(() => {
+				CustomGameEvents.FireEventToServer('EnableDebugBuy', new Map<string, any>(Object.entries({
+					player_id: LocalPlayer!.PlayerID,
+					debug_buy: allowBuy.value ? 1 : 0
+				})))
+			})
+
+			break
+		}
+	
+		default: {
+			break
+		}
+	}
+})
+
+EventsSDK.on('GameEnded', () => {
+	customGameNode?.DetachFromParent()
+})
+
+Events.on('CustomGameEvent', (name, data) => {
+	console.log('custom event', { name, data })
 })
